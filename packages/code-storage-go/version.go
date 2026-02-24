a3e3ac64b16e43e245eb6ba158846ca82c71f555
fat <jacobthornton@gmail.com>
2026-02-23 19:12:39 -0800

    bump versions

--- packages/code-storage-go/version.go
+++ packages/code-storage-go/version.go
@@ -1,7 +1,7 @@
 package storage
 
 const PackageName = "code-storage-go-sdk"
-const PackageVersion = "0.0.4"
+const PackageVersion = "0.1.0"
 
 func userAgent() string {
 	return PackageName + "/" + PackageVersion
